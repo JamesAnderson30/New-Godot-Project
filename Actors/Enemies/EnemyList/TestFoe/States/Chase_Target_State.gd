6cb03131acf53943033dc354678123454d32c22c
James Anderson <jamesanderson_18@hotmail.com>
2025-05-14 21:13:11 -0400

    finally got basic animation to fire

--- Actors/Enemies/EnemyList/TestFoe/States/Chase_Target_State.gd
+++ Actors/Enemies/EnemyList/TestFoe/States/Chase_Target_State.gd
@@ -3,12 +3,12 @@ extends Node
 var stateName = "chase_target"
 
 func enter():
-	print("Entered ", stateName, " state")
+	pass
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
--- Actors/Player/States/Running_Forward_State.gd
+++ Actors/Player/States/Running_Forward_State.gd
@@ -1,19 +1,15 @@
-extends CharacterBody3D
-class_name EnemyHandler
+extends Node
 
-var navTarget = null
-var hp = 100
-var gravity = 9.8
+var stateName = "running_forward"
 
-
-func setNavigationTarget(newTarget: Node):
-	navTarget = newTarget
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-#	We need to set velocity to zero
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+	
+func enter():
+	print("Entered Running State")
